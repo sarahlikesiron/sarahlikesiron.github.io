fd2a44497ea7436cc2bd500dfe7d36af659a87aa
Sarah Irons <59066345+sarahlikesiron@users.noreply.github.com>
2020-02-24 20:08:29 -0500

    Adding Current Files
    Adding the current status of the files to see what works/might cause issues

---
+++ assets/script/scripts.js
@@ -0,0 +1,20 @@
+var isDesign = true; // created to display the correct text and images
+// load text and images into arrays so that they are easy to 
+
+// functions to display text and color when the images are rolled over and off
+function displayText(num) {
+    // section to display the text on hover depending on what image is hovered over
+    document.getElementsByClassName("desc")[num].style.visibility = "visible";
+}
+function hideText(id) {
+    // section to hide the text once the mouse rolls off of the image
+}
+
+// functions to change whether the background images are for design or development depending on which is active (and set them active)
+function desOrDev() {
+    // set whichever link is active to its active state
+    
+    // change the background images and the text to match the correct link
+    
+    // reassign the variable
+}
