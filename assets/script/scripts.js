5ca333153593388851c20fc972e8b603da3d097e
Sarah Irons <59066345+sarahlikesiron@users.noreply.github.com>
2020-02-26 00:08:59 -0500

    more updates
    fixed images in all project files and then modified css, js, and sections of the other html files

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -1,16 +1,3 @@
-var button = document.getElementById("topBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    console.log(document.body.scrollTop);
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-}
-
 function toTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
